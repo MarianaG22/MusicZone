2921e0e45f17865258ba09d22b77bf0faaff2252
MarianaG22 <142903466+MarianaG22@users.noreply.github.com>
2025-04-30 14:38:23 -0500

    add - Crear urls.py para cada app y configuración de rutas en urls.py principal

--- MusicZone/MusicZone/urls.py
+++ MusicZone/MusicZone/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('autenticacion/', include('Authentication.urls')),
+    path('productos/', include('Inventory.urls')),
+    path('ventas/', include('Sales.urls')),
+    path('usuarios/', include('Users.urls')),
 ]
