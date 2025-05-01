f5667036961640a71fc81a278cd11ae7cc31ea78
MarianaG22 <142903466+MarianaG22@users.noreply.github.com>
2025-04-30 20:25:13 -0500

    add - Catálogo básico

--- MusicZone/Inventory/views.py
+++ MusicZone/Inventory/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Instrument, Type, Mark
 
-# Create your views here.
+def catalog(request):
+    instrument_list = Instrument.objects.all()
+    types = Type.objects.all()
+    marks = Mark.objects.all()
+
+    return render(request, 'catalog/catalog.html', {
+        'instrument_list': instrument_list,
+        'types': types,
+        'marks': marks
+    })
