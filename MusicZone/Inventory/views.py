75628f282985a036483994c45be40a64e1a9a9d3
MarianaG22 <142903466+MarianaG22@users.noreply.github.com>
2025-04-30 21:07:06 -0500

    add - Paginación para el catálogo

--- MusicZone/Inventory/views.py
+++ MusicZone/Inventory/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from .models import Instrument, Type, Mark
+from django.core.paginator import Paginator
 
 def catalog(request):
     instrument_list = Instrument.objects.all()
     types = Type.objects.all()
     marks = Mark.objects.all()
 
+    # Configurar paginación (mostrar 12 instrumentos por página)
+    paginator = Paginator(instrument_list, 12)  
+    page_number = request.GET.get('page')
+    instruments = paginator.get_page(page_number)
+
     return render(request, 'catalog/catalog.html', {
-        'instrument_list': instrument_list,
+        'instrument_list': instruments,
         'types': types,
         'marks': marks
     })
